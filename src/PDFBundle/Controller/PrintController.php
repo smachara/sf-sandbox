1ec665a973412a3877e91bfce0e619a0eed73d9a
smachara@gmail.com <smachara@gmail.com>
2016-03-13 21:52:55 +0100

    PRoblemas con la impresion   Posision de Div no aceptada   Cambiar todo por tablas   La imagen no quiere ser reconocida por pdf. posible solucion eliminat todo el HTML y dejar solola parte a imprimir.

--- src/PDFBundle/Controller/PrintController.php
+++ src/PDFBundle/Controller/PrintController.php
@@ -12,34 +12,73 @@ class PrintController extends Controller
      */
     public function printAction()
     {
-        $pdf = $this->get("white_october.tcpdf")->create();
 
-        // set margins
-        $pdf->SetMargins(PDF_MARGIN_LEFT,
-            PDF_MARGIN_TOP,
-            PDF_MARGIN_RIGHT);
-        $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
-        $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
-// set auto page breaks
-        $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
-// set image scale factor
-        $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
-// ---------------------------------------------------------
+        $pdf = $this->get("white_october.tcpdf")->create();
+//-------------------------------------------------
+//        (PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+// set document information
+//$pdf->SetCreator(PDF_CREATOR);
+//$pdf->SetAuthor('Nicola Asuni');
+//$pdf->SetTitle('TCPDF Example 061');
+//$pdf->SetSubject('TCPDF Tutorial');
+//$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+
 // remove default header/footer
         $pdf->setPrintHeader(false);
         $pdf->setPrintFooter(false);
 
-// set default font subsetting mode
-        $pdf->setFontSubsetting(false);
 
-// Add a page
-// This method has several options, check the source code documentation for more information.
-        $pdf->AddPage();
+/// set default header data
+//$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 061', PDF_HEADER_STRING);
 
-        $html = $this->renderView('PDFBundle:Print:print.html.twig', array());
-        dump($html);
-        die();
-        $pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+// set header and footer fonts
+//$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+//$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
+// set default monospaced font
+//$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+// set margins
+//$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+//$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+//$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+// set auto page breaks
+//$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+
+// set image scale factor
+$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+// ---------------------------------------------------------
+
+// set font
+//$pdf->SetFont('helvetica', '', 10);
+
+// add a page
+$pdf->AddPage();
+
+/* NOTE:
+ * *********************************************************
+ * You can load external XHTML using :
+ *
+ * $html = file_get_contents('/path/to/your/file.html');
+ *
+ * External CSS files will be automatically loaded.
+ * Sometimes you need to fix the path of the external CSS.
+ * *********************************************************
+ */
+
+// define some HTML content with style
+$html = $this->renderView('PDFBundle:Print:print.content.pdf.html.twig', array());
+        $pdf->writeHTML($html, true, false, true, false, '');
+        // reset pointer to the last page
+        $pdf->lastPage();
+//Close and output PDF document
+
+//        $pdf->Output('example_061.pdf', 'I');
+        return $this->render('PDFBundle:Print:print.content.pdf.html.twig', array());
+//-------------------------------------------------
     }
 
 }
