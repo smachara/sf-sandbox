76e2ed451835d6b4d9e968e5af00f16c2287bcfa
smachara <smachara@gmail.com>
2016-03-15 16:33:03 +0100

    Ninguna libtreria transforma HTML5 a PDF sin esfuerzo La solucion simple es HTML >> CANVAS/JPEG >> PDF
    Sin embargo no se ve muy nitido el PDF.

--- src/PDFBundle/Controller/PrintController.php
+++ src/PDFBundle/Controller/PrintController.php
@@ -14,25 +14,9 @@ class PrintController extends Controller
      */
     public function printAction()
     {
-        $pdf = $this->get("white_october.tcpdf")->create();
-        $pdf->setPrintHeader(false);
-        $pdf->setPrintFooter(false);
-
         $barcodeobj = new TCPDF2DBarcode('http://l.sandbox.com/app_dev.php/print-form', 'QRCODE,H');
-
         $qr = $barcodeobj->getBarcodeSVGcode( 3, 3, 'black');
-
-        $pdf->AddPage();
-
-    $html = $this->renderView('PDFBundle:Print:print.content.pdf.html.twig', array( 'preview' => false, 'qr' => $qr));
-            $pdf->writeHTML($html, true, false, true, false, '');
-            //$pdf->Output('example_001.pdf', 'I');
-     //       $response = new Response($pdf->Output('example_002.pdf','D'));
-     //       $response->headers->set('Content-Type', 'application/pdf');
-            //return $response;
-
-    return $this->render('PDFBundle:Print:print.content.pdf.html.twig', array( 'preview' => true, 'qr' => $qr));
-
+        return $this->render('PDFBundle:Print:print.html.twig', array( 'preview' => true, 'qr' => $qr));
     }
 
 }
